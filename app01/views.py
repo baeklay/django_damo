c0d41d49d6ef9b8d46d066b5b4b3d955cc2e5789
lay.j.yi <644969001@qq.com>
2023-12-11 14:22:15 +0800

    模板层之变量

--- app01/views.py
+++ app01/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    import datetime
+    s="hello"
+    l = [111, 222, 333]  # 列表
+    dic = {"name": "lay", "age": 18}  # 字典
+    date = datetime.date(2000, 2, 2)  # 日期对象
+
+    class Person(object):
+        def __init__(self, name):
+            self.name = name
+
+    person_lay = Person("lay")  # 自定义类对象
+    person_mona = Person("mona")
+    person_joker = Person("joker")
+
+    person_list = [person_mona, person_lay, person_joker]
+
+    return render(request, "index.html", {"l": l, "dic": dic, "date": date, "person_list": person_list})
